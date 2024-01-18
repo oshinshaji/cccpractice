e8e20809d420857242b7c4b407d86bd8bfed4b04
oshinshaji <oshinshaji501@gmail.com>
2024-01-18 15:41:38 +0530

    String Functions Practice

---
+++ string_exercise_5.php
@@ -0,0 +1,18 @@
+<?php
+echo "Exercise 5";
+echo "<br>";
+
+$quote = "In three words I can sum up everything I've learned about life: it goes on.";
+echo "Total number of words: ";
+echo str_word_count($quote);
+echo "<br>";
+
+echo "Lowercase: ";
+echo strtolower($quote);
+echo "<br>";
+
+echo "Capitilized first letter of each word: ";
+echo ucwords($quote);
+
+
+?>
