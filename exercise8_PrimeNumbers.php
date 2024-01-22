27299d250e2a9426a0084f2d04510e39d6b2385b
oshinshaji <oshinshaji501@gmail.com>
2024-01-22 14:29:10 +0530

    changes

--- exercise8_PrimeNumbers.php
+++ exercise8_PrimeNumbers.php
@@ -1,22 +1,27 @@
 <?php
-$n=20;
-$x=0;
-if($n==0 || $n==1){
-    echo "Neither Prime nor composite";
-}
-for($i=2;$i<=$n;$i++){
-   
-    if($n%$i == 0){
-        // echo "here"."<br>";
-        $x++;
+
+$num=20;
+function prime_number($n){
+    $x=0;
+    if($n==0 || $n==1){
+        echo "Neither Prime nor composite";
     }
-    
+    for($i=2;$i<=$n;$i++){
+       
+        if($n%$i == 0){
+            // echo "here"."<br>";
+            $x++;
+        }
+        
+    }
+    // echo $x;
+    if($x == 1){
+        echo "Prime";
+    }
+    else{
+        echo "Not Prime";
+    }
 }
-// echo $x;
-if($x == 1){
-    echo "Prime";
-}
-else{
-    echo "Not Prime";
-}
+prime_number($num);
+
 ?>
