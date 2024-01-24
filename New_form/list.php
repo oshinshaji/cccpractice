3d00059482f909570ab48366ca0a6a316f576a4a
oshinshaji <oshinshaji501@gmail.com>
2024-01-24 18:12:53 +0530

    changes

--- New_form/list.php
+++ New_form/list.php
@@ -1,23 +1,50 @@
 <?php
 include_once "form_connection.php";
-$sql="SELECT * FROM ccc_product;";
-// SELECT * FROM table_name
+// $sql="SELECT * FROM ccc_product order by sku ASC limit 10 EXCEPT TOP 2;";
+$maxInt = PHP_INT_MAX;
+$countQuery = "SELECT COUNT(*) AS total_rows FROM ccc_product";
+$countResult = $conn->query($countQuery);
+$countRow = $countResult->fetch_assoc();
+$totalRows = $countRow['total_rows'];
+$n=10;
+$limit=$totalRows-$n;
+
+$sql = "SELECT * FROM ccc_product
+        ORDER BY sku
+        LIMIT $limit , $n";
+
+$result = $conn->query($sql);
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "product: " . $row["product_name"]."&nbsp"."&nbsp"."&nbsp". 
+    echo 
+    "<ul>
+      <li>$row[product_name]
+      $row[sku].
+      $row[product_type].
+      $row[category].
+      $row[manufacturer_cost].
+      $row[shipping_cost].
+      $row[total_cost].
+      $row[price].
+      $row[status].
+      $row[created_at].
+      $row[updated_at].</li>
+      
+    </ul>";
+    /* "product: " . $row["product_name"]."&nbsp"."&nbsp"."&nbsp". 
     "sku: " . $row["sku"].
-    "product_type: " . $row["product_type"].
-    "category: " . $row["category"].
-    "manufacturer_cost: " . $row["manufacturer_cost"].
-    "shipping_cost: " . $row["shipping_cost"].
-    "total_cost: " . $row["total_cost"].
-    "price: " . $row["price"].
-    "status: " . $row["status"].
-    "created_at: " . $row["created_at"].
-    "updated_at: " . $row["updated_at"]."<br>";
+    ": " . $row[""].
+"ategory: "  $ro["category"].
+    "anufacture_cost: ". $row["manufacturer_cost"].
+    "hipping_cst: ". $row["shipping_cost"].
+    "otal_cos: ". $row["total_cost"].
+    "rice:  $row["price"].
+    "tatus:".$row["status"].
+    "reate_at:  .$row["created_at"].
+    "pdatd_at:  . row["updated_at"]."<br>"; */
   }
 } else {
   echo "0 results";
--- hourglass_pattern.php
+++ hourglass_pattern.php
@@ -6,6 +6,7 @@ for($i=1;$i<=$n/2;$i++){
     for($j=1;$j<=$n;$j++){
         if($j<$i || $j>$n-$i+1){
             echo "&nbsp"." ";
+            echo "&nbsp";
         }
         else{
             echo $j." ";
@@ -17,13 +18,14 @@ for($i=1;$i<=$n/2;$i++){
 /* for($i=$n/2;$i>0;$i--){
 
 } */
-for($i=($n/2)-2;$i>=0;$i--){
+for($i=($n/2)-1;$i>=0;$i--){
     for($j=1;$j<=$n;$j++){
         if($j>$i && $j<$n-$i+1){
             echo $j." ";
         }
         else{
             echo "&nbsp"." ";
+            echo "&nbsp";
         }
     }
     echo "<br>";
@@ -40,6 +42,7 @@ for($i=0;$i<$n/2;$i++){
     for($j=1;$j<=$n;$j++){
         if($j<=$i || $j>$n-$i){
             echo "&nbsp"." ";
+            echo "&nbsp";
         }
         else{
             echo $j." ";
@@ -51,6 +54,7 @@ for($i=1;$i<=$n/2;$i++){
     for($j=1;$j<=$n;$j++){
         if($j<=($n/2)-$i || $j>($n/2)+$i+1){
             echo "&nbsp"." ";
+            echo "&nbsp";
         }
         else{
             echo $j." ";
