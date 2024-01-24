34a7ced791f6d1575c4821102026ffeeb9fabf48
oshinshaji <oshinshaji501@gmail.com>
2024-01-24 09:34:10 +0530

    changes in file

--- pattern2.php
+++ pattern2.php
@@ -1,10 +1,10 @@
 <?php
 echo "<br>";
-$a=10;
-for($i=1; $i<= $a; $i++){
-    for($j=1; $j<=$a; $j++){
-        if($j<=($a+1)-$i){
-            echo("$j");
+$n=10;
+for($i=1; $i<= $n; $i++){
+    for($j=1; $j<=$n; $j++){
+        if($j<=($n+1)-$i){
+            echo $j." ";
         }
     }
     echo "<br>";
