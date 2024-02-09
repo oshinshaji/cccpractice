905d428b7a817ba0a3b47200832296907fbe1371
oshinshaji <oshinshaji501@gmail.com>
2024-02-09 15:11:18 +0530

    new folders and file

---
+++ NewMvc/app/code/local/Core/Block/Template.php
@@ -0,0 +1,20 @@
+<?php
+
+class Core_Block_Template{
+    public function toHtml(){
+
+    }
+
+    public function addChild($key, $value){
+
+    }
+
+    public function removeChild($key){
+
+    }
+
+    public function getChild($key){
+        
+    }
+}
+?>
